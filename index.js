7400ca8355aa95ab44283ee8ad82c749d66f5a40
ErenYaylaci <ErenYaylaci@DESKTOP-QUJIPHN>
2021-05-30 15:08:18 +0300

    master

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         let buttonInnerHtml = this.innerHTML;
-        switch (buttonInnerHtml) {
-            case "w":
-                soundPlay("tom-1")
-                break;
-            case "a":
-                soundPlay("tom-2")
-                break;
-            case "s":
-                soundPlay("tom-3")
-                break;
-            case "d":
-                soundPlay("tom-4")
-                break;
-            case "j":
-                soundPlay("snare")
-                break;
-            case "k":
-                soundPlay("crash")
-                break;
-            case "l":
-                soundPlay("kick-bass")
-                break;
-        }
+        makeSound(buttonInnerHtml);
     });
 }
+document.addEventListener("keypress",function (event) {
+    var key = event.key;
+    makeSound(key);  
+});
 
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            soundPlay("tom-1")
+            break;
+        case "a":
+            soundPlay("tom-2")
+            break;
+        case "s":
+            soundPlay("tom-3")
+            break;
+        case "d":
+            soundPlay("tom-4")
+            break;
+        case "j":
+            soundPlay("snare")
+            break;
+        case "k":
+            soundPlay("crash")
+            break;
+        case "l":
+            soundPlay("kick-bass")
+            break;
+    }
+}
 
 function soundPlay(name) {
     var audio = new Audio("./sounds/"+name+".mp3");
     audio.play();
 }
-      
+   
